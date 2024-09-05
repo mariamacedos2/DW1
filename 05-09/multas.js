e9ec25cac1d2021f1bb5248ff22991eb2628d7b1
Maria Eduarda <163424987+mariamacedos2@users.noreply.github.com>
2024-09-05 11:49:46 -0300

    hh

---
+++ 05-09/multas.js
@@ -0,0 +1,6 @@
+let inputmaxima = document.getElementById("maxima")
+let inputveiculo = document.getElementById("veiculo")
+let inputcalcular = document.getElementById("calcular")
+
+let Calcular = document.getElementById("calcular")
+Calcular.onclick = calculaImc 
