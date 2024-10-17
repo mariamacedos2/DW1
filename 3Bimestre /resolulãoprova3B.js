2b672778556b44f6525fea6f351dad79e4800021
Maria Eduarda <163424987+mariamacedos2@users.noreply.github.com>
2024-10-17 11:57:48 -0300

    p

---
+++ 3Bimestre /resolulãoprova3B.js
@@ -0,0 +1,57 @@
+function somaSerie(cont){
+    let cont = 3
+
+    let soma = 0
+    let y = 1
+    for(let x=1, y = 1; x<=cont; x++, y +=2){
+        soma =  soma + (x * y)
+        y += 2
+    }
+
+    console.log(soma)
+
+    return soma
+}
+
+
+//function somaSerie(cont){
+//    let soma = 0
+
+    
+
+//    return soma
+//}
+
+//console.log(somaSerie(3))
+//console.log(somaSerie(4))
+
+
+
+
+//cont = 4
+//               x   y
+//soma =  soma + (1 * 1)
+//soma =  soma + (2 * 3)
+//soma =  soma + (3 * 5)
+//soma =  soma + (4 * 7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//exemplo 2
+//                x   x+1
+//soma =  soma + (1 * 1)
+//soma =  soma + (2 * 3)
+//soma =  soma + (3 * 5)
+//soma =  soma + (4 * 7)
