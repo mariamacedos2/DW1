7baa4a6af6634b652c7ba8fe814c6be91ffe8917
Maria Eduarda <163424987+mariamacedos2@users.noreply.github.com>
2024-10-31 11:47:16 -0300

    .

---
+++ 4Bimestre/31-10/arrays2.js
@@ -0,0 +1,15 @@
+let vetor = [10,20,30]
+
+console.log(vetor)
+
+vetor[3] = 100
+console.log(vetor)
+
+vetor[4] = 200
+console.log(vetor)
+
+vetor[vetor.length] = 200
+console.log(vetor)
+
+vetor.unshift(5)
+console.log(vetor)
